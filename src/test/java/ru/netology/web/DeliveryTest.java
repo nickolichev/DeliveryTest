9993a1298d5203f4a119498220ec706393883cfa
nickolichev@gmail.com <nickolichev@gmail.com>
2022-08-09 11:24:16 +0300

    first

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -1,8 +1,7 @@
 package ru.netology.web;
 
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
